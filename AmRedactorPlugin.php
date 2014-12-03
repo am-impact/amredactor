fce61f2e9f715f447accdfc22ff5a5cac15e10e8
Hubert Prein <h.prein@am-impact.nl>
2014-12-03 12:29:51 +0100

    Revert "Add transformations to assets when an entry is saved"
    This reverts commit 579c6851655448b36f5fbb452c5f088d118574c7.

--- AmRedactorPlugin.php
+++ AmRedactorPlugin.php
@@ -3,82 +3,31 @@ namespace Craft;
 
 class AmRedactorPlugin extends BasePlugin
 {
-	public function getName()
-	{
-		 return 'a&m impact redactor';
-	}
+    public function getName()
+    {
+         return 'a&m impact redactor';
+    }
 
-	public function getVersion()
-	{
-		return '1.0';
-	}
+    public function getVersion()
+    {
+        return '1.0';
+    }
 
-	public function getDeveloper()
-	{
-		return 'a&m impact';
-	}
+    public function getDeveloper()
+    {
+        return 'a&m impact';
+    }
 
-	public function getDeveloperUrl()
-	{
-		return 'http://www.am-impact.nl';
-	}
+    public function getDeveloperUrl()
+    {
+        return 'http://www.am-impact.nl';
+    }
 
-	public function init()
-	{
-		if(craft()->request->isCpRequest())
-		{
-			craft()->templates->includeJsResource('amredactor/js/amredactor.js');
-		}
-
-		if (craft()->request->isPostRequest() && craft()->request->isCpRequest())
-		{
-			craft()->plugins->getPlugin('amredactor')->transformImages();
-		}
-	}
-
-	private function transformImages()
-	{
-		craft()->on('entries.beforeSaveEntry', function(Event $event)
-		{
-			$entry = $event->params['entry'];
-			foreach ($entry->getFieldLayout()->getFields() as $fieldLayout)
-			{
-				$field = $fieldLayout->getField();
-				$fieldType = $field->getFieldType();
-				if ($fieldType instanceof RichTextFieldType)
-				{
-					$richText = $entry->getContent()->getAttribute($field->handle);
-					if (!empty($richText))
-					{
-						$doc = new \DOMDocument();
-						libxml_use_internal_errors(true);
-						$doc->loadHTML($richText);
-						$images = $doc->getElementsByTagName('img');
-
-						foreach ($images as $image)
-						{
-							$assetPartsString = substr($image->getAttribute('src'), 1, count($image->getAttribute('src')) - 2);
-							$sourceParts = explode(':', $assetPartsString);
-							$dimensions = craft()->amRedactor->getDimensionsFromStyle($image->getAttribute('style'));
-							$transformWithoutDimensions = ($sourceParts[2] != 'url' && $dimensions['height'] == 'auto' && $dimensions['width'] == 'auto');
-
-							// Don't do anything if a transform is already set, but no dimensions were specified.
-							if (!$transformWithoutDimensions)
-							{
-								$assetTransformHandle = craft()->amRedactor->getNearestTransform($dimensions);
-
-								if ($assetTransformHandle !== false)
-								{
-									$sourceParts[2] = $assetTransformHandle;
-								}
-								$image->setAttribute('src', '{' . implode(':', $sourceParts) . '}');
-							}
-						}
-
-						$entry->getContent()->setAttribute($field->handle, urldecode($doc->saveHTML()));
-					}
-				}
-			}
-		});
-	}
+    public function init()
+    {
+        if(craft()->request->isCpRequest())
+        {
+            craft()->templates->includeJsResource('amredactor/js/amredactor.js');
+        }
+    }
 }
