fe8fe1b690417cf42a692ec77639f36e34f5a3b9
Lennard Geven <l.geven@am-impact>
2017-05-08 16:14:23 +0200

    Mogelijkheid toegevoegd om opmaak te verwijderen van geselecteerde tekst

--- resources/js/amredactor.js
+++ resources/js/amredactor.js
@@ -25,34 +25,42 @@ RedactorPlugins.amredactor = function() {
             var button = this.button.addBefore('bold', 'clear', 'Remove all styles');
             
             this.button.addCallback(button, function() {
+
                 if( confirm('Are you sure that you want to remove all styles?') ) {
-                    /**
-                     * Add spaces at end of paragraphs, so content won't be glued to each other
-                     */
-                    var paragraphs = this.$editor[0].querySelectorAll('p');
-                    
-                    // Loop through the paragraphs
-                    for( var p = 0, plen = paragraphs.length; p < plen; p++ ) {
-                        // Get the plain html from the paragraph
-                        var paragraphText = this.clean.stripTags( paragraphs[p].innerHTML );
+
+                   // If there is text selected format only this part of the text
+                   if ( this.selection.getHtml().length > 0 ) {
+                        var selectedHtml = this.selection.getHtml();
+                        this.insert.text( this.clean.getPlainText(selectedHtml) ) ;
+                    } else {
+                        /**
+                         * Add spaces at end of paragraphs, so content won't be glued to each other
+                         */
+                        var paragraphs = this.$editor[0].querySelectorAll('p');
                         
-                        // When the last character of the paragraph isn't a space and when it isn't the last paragraph
-                        // then add a space
-                        if( paragraphText[ paragraphText.length - 1 ] !== ' ' && p < plen - 1 ) {
-                            paragraphs[p].innerHTML = paragraphs[p].innerHTML += ' ';
+                        // Loop through the paragraphs
+                        for( var p = 0, plen = paragraphs.length; p < plen; p++ ) {
+                            // Get the plain html from the paragraph
+                            var paragraphText = this.clean.stripTags( paragraphs[p].innerHTML );
+                            
+                            // When the last character of the paragraph isn't a space and when it isn't the last paragraph
+                            // then add a space
+                            if( paragraphText[ paragraphText.length - 1 ] !== ' ' && p < plen - 1 ) {
+                                paragraphs[p].innerHTML = paragraphs[p].innerHTML += ' ';
+                            }
                         }
+
+                        /**
+                        * this.content cannot be used because it doesn't update when typing
+                        * So get the html with innerHTML
+                        */
+                        var newContent = this.clean.stripTags( this.$editor[0].innerHTML );
+
+                        // Delete all newlines
+                        newContent = newContent.replace( /\r?\n|\r/g, ' ' );
+
+                        this.insert.set( newContent );
                     }
-
-                    /**
-                     * this.content cannot be used because it doesn't update when typing
-                     * So get the html with innerHTML
-                     */
-                    var newContent = this.clean.stripTags( this.$editor[0].innerHTML );
-
-                    // Delete all newlines
-                    newContent = newContent.replace( /\r?\n|\r/g, ' ' );
-
-                    this.insert.set( newContent );
                 }
             });
         },
